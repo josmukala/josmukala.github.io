2bf967da4331dc06c4d87970ccee51c12a2bbb3d
Joanna Smukała <josmukala@gmail.com>
2019-08-03 10:35:51 +0200

    javascript

---
+++ JS/FUNKCJE/js/functions.js
@@ -0,0 +1,13 @@
+function dodawanie (x,y) {
+    let wynik;
+    wynik = (x+y)*2;
+    return wynik;
+}
+console.log(dodawanie(5,6))
+
+let odejmowanie = (a,b) => {
+    let roznica;
+    roznica = a - b
+    return roznica;
+}
+console.log(odejmowanie(50,18))
