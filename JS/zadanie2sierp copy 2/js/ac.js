0161ce02d464c2171dc0a6c7a1bcba4f94eff307
Joanna Smukała <josmukala@gmail.com>
2019-08-04 16:41:12 +0200

    aa

--- JS/zadanie2sierp copy 2/js/ac.js
+++
@@ -1,7 +0,0 @@
-console.log(document.getElementById("formularz").lastElementChild)
-document.getElementById("formularz").lastElementChild.addEventListener('click', function(e){
-    e.preventDefault();
-    let fname =  document.getElementsByTagName("input")[0].value
-    let lname = document.getElementsByTagName("input")[1].value
-    console.log("jego imie to:" + fname + "a nazwisko to:" + lname)
-})
