ac1916cc879a9dfc7d328d3b86c463006c792d0d
Joanna Smukała <josmukala@gmail.com>
2019-08-04 14:04:53 +0200

    zadanie2sierp

---
+++ JS/zadanie2sierp/js/ab.js
@@ -0,0 +1,7 @@
+function ustawTlo() {
+    document.getElementById( "jeden" ).style.backgroundColor="red";
+    document.getElementById("dwa").style.backgroundColor="yellow";
+
+    }
+    document.getElementById( "btn" ).onclick = ustawTlo;
+    
